38fe07c1abd78a27334c43ef6d7e5e381a41af82
deveshnykaa <devesh.gupta@nykaa.com>
2021-01-20 11:09:05 +0530

    First Commit

--- src/main/java/com/api/mktplace/nyk_marketplace/controller/InventoryController.java
+++ src/main/java/com/api/mktplace/nyk_marketplace/controller/InventoryController.java
@@ -1,10 +1,15 @@
 package com.api.mktplace.nyk_marketplace.controller;
 
-import com.api.mktplace.nyk_marketplace.entity.Seller;
+import com.api.mktplace.nyk_marketplace.config.MessagingConfig;
+import com.api.mktplace.nyk_marketplace.converter.InventoryConverter;
+import com.api.mktplace.nyk_marketplace.dto.InventoryDTO;
+import com.api.mktplace.nyk_marketplace.entity.Inventory;
 import com.api.mktplace.nyk_marketplace.service.InventoryService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,10 +18,31 @@ import java.util.List;
 public class InventoryController {
 
     @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
     private InventoryService inventoryService;
 
-    @PutMapping("/updateInventory")
-    public void updateInven(@RequestBody List<Seller> seller){
-        this.inventoryService.updateInven(seller);
+    @Autowired
+    private InventoryConverter inventoryConverter;
+    //private static final Logger LOG = LogManager.getLogger(InventoryController.class);
+
+    @PutMapping("/v2/updateInventory")
+    public String updateInventory(@RequestHeader(value = "Authorization") String apiKey , @RequestBody List<InventoryDTO> inventoryDTOS){
+        //LOG.info("UpdateInventory Request sent with RequestBody "+vendors);
+        List<Inventory> inventories = inventoryConverter.dtoToEntity(inventoryDTOS);
+
+        template.convertAndSend(MessagingConfig.EXCHANGE, MessagingConfig.ROUTING_KEY_1, inventories);
+        //template.convertSendAndReceive()
+        // template.convertAndSend(MessagingConfig.EXCHANGE, MessagingConfig.ROUTING_KEY_1, apiKey, inventories);
+        return "Order taken!!";
+        //this.inventoryService.updateInventory(apiKey, inventories);
+    }
+
+    @PutMapping("/v3/inventoryPriceUpdate")
+    public void inventoryPriceUpdate(@RequestHeader(value = "Authorization") String apiKey , @RequestBody List<InventoryDTO> inventoryDTOS){
+        //LOG.info("InventoryPriceUpdate Request sent with RequestBody "+sellers);
+        List<Inventory> inventories = inventoryConverter.dtoToEntity(inventoryDTOS);
+        this.inventoryService.inventoryPriceUpdate(apiKey, inventories);
     }
 }
